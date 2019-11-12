8c30750e11a130244214948042ad7ade1c8d76bb
Sarat Deevi <sarat.deevi@jda.com>
2019-11-12 13:44:42 +0530

    remove application home page open

--- src/main/java/com/swetha/pricing/Application.java
+++ src/main/java/com/swetha/pricing/Application.java
@@ -3,29 +3,24 @@ package com.swetha.pricing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.awt.*;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
 
-        openHomePage();
+        //openHomePage();
     }
 
-    private static void openHomePage() {
-        try {
-            String port = System.getProperty("server.port");
-            System.out.println("Port=" + port);
-
-            URI homepage = new URI("http://localhost:/" + port);
-            Desktop.getDesktop().browse(homepage);
-        } catch (URISyntaxException | IOException e) {
-            e.printStackTrace();
-        }
-    }
+//    private static void openHomePage() {
+//        try {
+//            String port = System.getProperty("server.port");
+//            System.out.println("Port=" + port);
+//
+//            URI homepage = new URI("http://localhost:/" + port);
+//            Desktop.getDesktop().browse(homepage);
+//        } catch (URISyntaxException | IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
